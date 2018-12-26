ee0c4053bd31da456711b604ab9bc8d8941c3fe1
Sophia Mai <phuongmai001@gmail.com>
2018-12-25 19:33:33 -0800

    add type annotations for variables

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
+const symbol: string = "#";
+const hashLength: number = 7;
+
 function generateRandomId() {
-  return "#" + Math.random().toString(36).substr(2, 7);
+  return symbol + Math.random().toString(36).substr(2, hashLength);
 }
 
 function main() {
-  const app = document.getElementById('app');
+  const app = document.getElementById("app");
   setInterval(function() {
     app.innerHTML = generateRandomId();
   }, 1000);
--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,11 @@
 "use strict";
+var symbol = "#";
+var hashLength = 7;
 function generateRandomId() {
-    return "#" + Math.random().toString(36).substr(2, 7);
+    return symbol + Math.random().toString(36).substr(2, hashLength);
 }
 function main() {
-    var app = document.getElementById('app');
+    var app = document.getElementById("app");
     setInterval(function () {
         app.innerHTML = generateRandomId();
     }, 1000);

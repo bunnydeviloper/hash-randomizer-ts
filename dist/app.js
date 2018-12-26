41de578dca2694e6a614080b6ffb9d6074a9041d
Sophia Mai <phuongmai001@gmail.com>
2018-12-25 19:15:30 -0800

    turn regular app to typescript app;

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+function generateRandomId() {
+    return "#" + Math.random().toString(36).substr(2, 7);
+}
+function main() {
+    var appComponent = document.getElementById('app');
+    setInterval(function () {
+        appComponent.innerHTML = generateRandomId();
+    }, 1000);
+}
